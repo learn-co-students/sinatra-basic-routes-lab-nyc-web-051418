96f0f63fa134622482ab20fe5805baa0a75a8bbd
David Tenenbaum <github email address>
2018-06-04 10:49:47 -0400

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is David Tenenbaum"
+  end
+
+  get '/hometown' do
+    "My hometown is Tel-Aviv"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Someone To Watch Over Me"
+  end
+
 end

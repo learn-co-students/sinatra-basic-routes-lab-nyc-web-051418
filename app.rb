973f73629faba1e0eea1a56ad77926e2cdf86af8
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-04 16:04:11 -0400

    Solved lab

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get ('/') { "Hello, World!" }
+  get ('/name') { "My name is Alberto" }
+  get ('/hometown') { "My hometown is Palleja" }
+  get ('/favorite-song') { "My favorite song is Lalala" }
+
 end
